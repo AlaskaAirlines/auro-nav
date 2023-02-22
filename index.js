5be279b408cf709c84878fad5bb903730e5aa439
Jordan Jones <jmjones2463@gmail.com>
2023-02-22 09:27:25 -0800

    style: use auro variables

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { AuroNav } from './src/auro-nav.js';
 
 /**
  * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
+ * @param {String} name - Name to use for custom element.
+ * @param {Function} className - Class name to use for custom element.
  * @returns {void}
  */
 export function registerComponent(name, className) {
